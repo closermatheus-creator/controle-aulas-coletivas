58c29e8ebcd7a5274f4300241d7a02c985256f19
closermatheus-creator <closermatheus@gmail.com>
2026-07-03 18:36:49 -0300

    okfirebase

---
+++ script-firebase-antigo.js
@@ -0,0 +1,12 @@
+{
+  "dataExportacao": "2026-07-03T15:30:00.000Z",
+  "alunos": [ ... ],
+  "experimentais": [ ... ],
+  "historico": [ ... ],
+  "turmas": [ ... ],
+  "modalidades": [ ... ],
+  "totalAlunos": 150,
+  "totalExperimentais": 23,
+  "totalHistorico": 45,
+  "totalTurmas": 73
+}
